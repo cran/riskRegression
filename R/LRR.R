5406c8d4a335f7aa38c21c3e0b3ad06fd93baf0c
Thomas Alexander Gerds <tag@biostat.ku.dk>
2011-11-17 00:00:00 +0000

    version 0.0.5

---
+++ R/LRR.R
@@ -0,0 +1,12 @@
+LRR <- function(formula,data,times,cause,cens.model,cens.formula,...){
+  fit <- riskRegression(formula=formula,
+                        data=data,
+                        times=times,
+                        link="logistic",
+                        cause=cause,
+                        cens.model=cens.model,
+                        cens.formula=cens.formula,
+                        ...)
+  fit$call <- match.call()
+  fit
+}
